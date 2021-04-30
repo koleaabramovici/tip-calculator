e90a5cd4f8c8258afe7b993a5142212d82a086b7
koleaabramovici <78415812+koleaabramovici@users.noreply.github.com>
2021-04-30 10:59:27 +0300

    tip calculator

--- file.js
+++ file.js
@@ -1,12 +1,12 @@
+document.getElementById('calculate').onclick = function(){
 let totalBill = document.getElementById('bill').value;
 let serviceLvl = document.getElementsByName('tip');
 let guests = document.getElementById('people');
 let tip = document.getElementById('tip');
 
-document.getElementById('calculate').onclick = function(){
 for(let i=0; i < serviceLvl.length; i++){
   if(serviceLvl[i].checked){
-    tip.innerHTML = (totalBill * serviceLvl[i]) / guests;
+    tip.innerHTML = (totalBill * serviceLvl[i].value) / guests.value;
   }
  }
 };
